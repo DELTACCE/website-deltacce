f2aef742ca58a56778b1c1145afb637d6a26c199
John Antony <133796114+KNOWASJOHN@users.noreply.github.com>
2026-08-10 21:34:26 +0530

    an unused variable lead to deployment cancel in app.js (useLocation was unused)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import CustomCursor from './components/CustomCursor';
 import Navbar from './components/Navbar';
